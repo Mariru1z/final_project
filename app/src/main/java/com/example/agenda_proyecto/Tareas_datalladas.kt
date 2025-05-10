b94c5b2c7a349f4d3ebb34098f560a2d68d7a121
Maria Ruiz <maria.ruizsa@anahuac.mx>
2025-05-10 11:54:46 -0600

    login

--- app/src/main/java/com/example/agenda_proyecto/Tareas_datalladas.kt
+++ app/src/main/java/com/example/agenda_proyecto/Tareas_datalladas.kt
@@ -1,30 +1,21 @@
 package com.example.agenda_proyecto
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Tareas_datalladas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tareas_datalladas)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Acción del botón para abrir la nueva Activity
-        val botonIniciar = findViewById<Button>(R.id.B1)
-        botonIniciar.setOnClickListener {
-            val intent = Intent(this, Tareas_datalladas::class.java)
-            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/agenda_proyecto/Tareas_generales.kt
+++ app/src/main/java/com/example/agenda_proyecto/Tareas_generales.kt
@@ -1,30 +1,20 @@
 package com.example.agenda_proyecto
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Tareas_generales : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_tareas_generales)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Acción del botón para abrir la nueva Activity
-        val botonIniciar = findViewById<Button>(R.id.B1)
-        botonIniciar.setOnClickListener {
-            val intent = Intent(this, Tareas_datalladas::class.java)
-            startActivity(intent)
         }
     }
 }
